cb12940d6d02326b4967bd85b31c96c1e029c699
James Murphy <jamesthebald@gmail.com>
2021-05-11 16:56:27 -0600

    Resume website ready to go live

--- public/vue_data.js
+++ public/vue_data.js
@@ -51,8 +51,8 @@ var vm2 = new Vue({
         altText: "Initial design sketches",
         caption: "My initial layout sketch for the top part of this webpage",
         description:
-          "The website you're currently looking at is my first website ever. It's built with straightforward HTML but with Tailwind for formatting and Vue.js for loading in the content. I went for a simple, clean look, and worked hard to make it fully mobile responsive. (Try resizing this window!)",
-        features: "Features: Javascript for navbar opening/closing, and Vue.js for content loading",
+          "The website you're currently looking at is my first website ever. It's straightforward HTML but with Tailwind for formatting, JS for controls and Vue.js for loading the content. I went for a simple, clean look, and worked hard to make it fully mobile responsive. (Try resizing this window.)",
+        features: "Future Features: More interesting colour scheme; display resume by rendering PDF; add a page/features that shows my interests and personality",
         languages: "Languages/Frameworks: HTML, CSS, Tailwind, Javascript, Vue.js",
         link1Text: "Home",
         link1: "./index.html",
@@ -67,7 +67,7 @@ var vm2 = new Vue({
         caption: "A screenshot from SwarmShare",
         description:
           "This app allows easy sharing of diverse information (such as learning resources), using a '2.5-dimensional mindmap' format. Users navigate to topics of interest by zooming and panning. They can then open and read postings, or create, edit or move them. This app makes use of hooks like useState, useRef and useEffect, as well as modals, tooltips, listeners. It leverages numerous React libraries, including react-zoom-pan-pinch, react-draggable, and suneditor-react.",
-        features: "Features: zoom-pan navigation, drag-and-drop items, rich text editor",
+        features: "Key Features: zoom-pan navigation, drag-and-drop organization, rich text editor",
         languages: "Javascript, React, Tailwind CSS, Express, Axios, Node.js, mongoDB",
         link1Text: "'Run It' coming soon",
         link1: "./projects.html#proj2",
